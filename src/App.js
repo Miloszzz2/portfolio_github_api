e9aa49ba079e6a4188f4c34ad82a76267bfa75e6
Miloszzz2 <97192271+Miloszzz2@users.noreply.github.com>
2022-10-14 23:46:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
+  const [user, setData] = useState();
+  const [repos, setRepos] = useState();
+  useEffect(() => {
+    axios
+      .get('https://api.github.com/users/Miloszzz2')
+      .then((response) => setData(response.data));
+    axios
+      .get('https://api.github.com/users/Miloszzz2/repos')
+      .then((response) => {
+        setRepos(response.data);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {user && (
+        <>
+          <div className='user_card '>
+            {<img src={user.avatar_url} alt='user_profile' />}
+            <h1>{user.login}</h1>
+            <p>{user.bio}</p>
+            <p>Followers: {user.followers}</p>
+            <p>Public repos : {user.public_repos}</p>
+            <a href={user.html_url} target='blank'>
+              Go to profile
+            </a>
+          </div>
+        </>
+      )}
+      {repos && (
+        <>
+          <div className='repos_card'>
+            <h1>Public Repositories:</h1>
+
+            {repos.map((item) => {
+              return (
+                <div className='repo'>
+                  <p>{item.name}</p>
+                  <p>Created at: {item.created_at}</p>
+                  <p>
+                    Language:{' '}
+                    {item.language != null ? item.language : 'Readme.md'}
+                  </p>
+                  <p>Watchers: {item.watchers}</p>
+                  <a href={item.html_url}>Go to</a>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
